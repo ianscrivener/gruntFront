e7aa5da2b46825606e0e7e7393d3cca579c7d132
Ian Scrivener <github@zilogy.asia>
2014-01-23 22:08:28 +1100

    init

---
+++ Gruntfile.js
@@ -0,0 +1,204 @@
+module.exports = function (grunt) {
+	
+	grunt.initConfig({
+		
+		// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+		// compile Jade >> HTML
+		jade: {
+			development: {
+				options: {
+					pretty: true,
+					data: {
+						debug: true
+					}
+				},
+				files: {
+					"wwwroot/index.html": "source/jade/index.jade"
+				}
+			},
+			production: {
+				options: {
+					data: {
+						debug: false
+					}
+				},
+				files: {
+					"wwwroot/index.html": "source/jade/index.jade"
+				}
+			}
+		}
+		,		
+		
+		// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&		
+		// process LESS tempaltes >> CSS
+		less: {
+			development: {
+				options: {
+					paths: ["source/less"]
+				},
+				files: {
+					"wwwroot/css/less.css": "source/less/less.less"
+				}
+			}
+			,
+			
+			production: {
+				options: {
+					paths: ["/source/less"],
+					compress: true,
+					yuicompress: true,
+					optimization: 1,
+					cleancss: true
+				},
+				files: {
+					"wwwroot/css/less.css": "source/less/less.less"
+				}
+			}			
+		}
+		,
+
+		// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&		
+		// comile sass >> css
+		compass: {
+			development: {
+				options: {
+					sassDir: 'source/sass',
+					cssDir: 'wwwroot/css',
+					outputStyle: 'expanded'
+				}
+			}
+			,
+			production: {
+				options: {
+					sassDir: 'source/sass',
+					cssDir: 'wwwroot/css',
+					outputStyle: 'compressed'
+				}
+			}
+			
+		}
+		,
+
+		// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&		
+		//copies files around.. mostly used for assets  
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'source',
+					dest: 'wwwroot',
+					src: ['img/**']
+				}]
+			}
+		}
+		,
+		
+		// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&		
+		//create a local webserver http://localhost:5455  
+		connect: {
+			server: {
+				options: {
+					port: 8080,
+					base: 'wwwroot',
+					livereload: true
+				}
+			}
+		}
+		,
+
+		// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&				
+        // clean destination folder
+        clean: {
+            wwwroot: [
+                '!github/do-not-touch',
+                '!github/.git/**',
+                '!github/CNAME',
+                '!README.md',
+                'wwwroot/**/*'
+            ]
+        }
+		,
+		
+		// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&		
+		//watches for changes to code then execute other Grunt processes  
+		watch: {
+			
+			// watch for Jade changes
+			jade: {
+				files: ['source/jade/**'],
+				tasks: ['jade']
+			}
+
+			,
+			//watch for SASS changes	
+			sass: {
+				files: ['source/sass/**'],
+				tasks: ['compass']
+			}
+			
+			,
+			//watch for LESS changes	
+			less: {
+				files: ['source/less/**'],
+				tasks: ['less']
+			}
+			
+			,					
+			// watch for other files that need to be copied	
+			copy: {
+				files: ['source/img/**'],
+				tasks: ['copy']
+			}
+			,
+			
+			// watch for any changes in destination (wwwroot) and do a browser reload
+			reload: {
+				files: ['wwwroot/**'],
+				options: {
+					livereload: true
+				}
+			}
+			
+		}
+		,
+		
+		// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&			
+		// open the dev site in the browser 
+		open: {
+			github: {
+				path: 'http://localhost:8080'
+			}
+		},
+			
+	});
+	
+	require('load-grunt-tasks')(grunt);
+	
+	
+	grunt.registerTask('default', [
+		'clean',
+		'jade:development',
+		'less:development',
+		'compass:development',
+		'copy',
+		'connect', 
+		'open',
+		'watch'
+	]);
+
+	grunt.registerTask('clean', ['clean']);	
+	
+	grunt.registerTask('production', [
+		'clean',
+		'jade:production',
+		'less:production',
+		'compass:production',
+		'copy',
+		'connect', 
+		'open',
+		'watch'
+	]);
+	
+	
+	require('load-grunt-tasks')(grunt);
+};
--- README.md
+++ README.md
@@ -1,4 +1,105 @@
-starter4Russ
-============
+#starter4Russ
+
+
+###Introduction
 
 A Jade, LESS &amp; SASS, Grunt.js starter pack for front-end designers/developers
+
+
+
+####1) Install Git
+
+1.1) Mac (you need MacPorts already installed). Run the following command in **Terminal**
+
+
+    sudo port install git-core +svn +doc +bash_completion +gitweb 
+
+
+1.2) To test your Git install run the following command in **Terminal** 
+
+    Download & Install the Node.js Mac OSX Installer
+    
+You should see something like this
+
+    git version 1.8.3.2
+    
+
+####2) Install node.js via Mac OSX Installer
+
+2.1) Uninstall any previous Node.js versions you may already have
+
+
+2.2) Download & Install the Node.js Mac OSX Installer - [http://nodejs.org/dist/v0.10.23/node-v0.10.23.pkg](http://nodejs.org/dist/v0.10.23/node-v0.10.23.pkg "")
+
+2.3) To ensure the correct permissions are set by running the following in **Terminal**:
+
+    sudo chown -R $USER /usr/local
+
+
+2.4) Test your node.js install;
+
+    node -v
+    
+you should see something like
+
+    v0.10.24
+
+2.5) node.js comes with an excellent package manager called **npm**. Next test it to see that its working OK:
+
+    npm -v
+    
+you should see something like this;
+
+    1.3.21
+    
+####3) Install Grunt.js
+
+In **Terminal** run;
+
+    npm install -g grunt-cli
+
+
+####4) Grab the demo code from GitHub
+
+In **Terminal** run;
+
+    git clone https://github.com/ianscrivener/starter4Russ
+    cd start4Russ
+    ls -l
+
+This will create a new directory called 'starter4Russ'.. something like this
+
+    | output
+        |css
+        | img
+        index.html
+    |source
+        |img
+        |jade
+        |less
+        |sass
+        index.jade
+    Gruntfile.js
+    package.json
+    README.md        
+    
+####5) Install the projects
+
+
+In **Terminal** run;
+    
+    npm init
+    
+This will download & install all the Grunt.js packages requiredfor our project.
+
+####6) Start grunt
+
+In **Terminal** run;
+    
+    npm init
+
+
+    
+
+
+
